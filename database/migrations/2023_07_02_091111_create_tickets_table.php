d1ee76c15caf794446e965dbbd68e495eaae0952
MMahdiSheykh <momoaban@gmail.com>
2023-07-02 15:04:40 +0330

    completing ORM of ticket migration table

--- database/migrations/2023_07_02_091111_create_tickets_table.php
+++ database/migrations/2023_07_02_091111_create_tickets_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Enums\TicketStatus;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -15,10 +16,11 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->text('description');
-            $table->enum('status',['open','resolved','rejected'])->default('open');
+            $table->string('status')->default(TicketStatus::OPEN->value);
             $table->string('attachment')->nullable();
-            $table->integer('user_id');
-            $table->integer('status_changed_by_id')->nullable();
+            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('status_changed_by_id')->nullable()->constrained('users');
+            $table->timestamps();
         });
     }
 
